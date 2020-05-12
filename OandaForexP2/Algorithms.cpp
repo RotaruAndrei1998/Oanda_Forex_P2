25df3b5acbdb458ad19cbfb2d1db32b59aa369fb
Andrei Rotaru <andreirotaru1998@gmail.com>
2020-05-13 02:11:30 +0300

    Added polynomial regression and refactor.

--- OandaForexP2/Algorithms.cpp
+++ OandaForexP2/Algorithms.cpp
@@ -1,30 +1,29 @@
 #include "Algorithms.h" 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector <double> Function_SMA(vector<double>& arr, double period) {
     double sum = 0;
     vector <double> average;
-    int j = arr.size();
-
-    for (int i = 0; i < j; i++) {
-
+    int i;
+    for (i = 0; i < arr.size(); i++) {
         sum += arr[i];
-
         if (i >= period)  sum -= arr[i - period];
-
-
         average.push_back(sum / period);
-
     }
+    sum += average[average.size() - 1];
+    sum -= arr[i - period];
+   
+    average.push_back(sum / period);
 
     return average;
 }
 
 
-vector < double> Function_EMA(vector<double>& arr, double period) {
+vector <double> Function_EMA(vector<double>& arr, double period) {
     int j = arr.size();
     vector <double> average;
     double weight = (2.0 / (period + 1));
@@ -33,30 +32,22 @@ vector < double> Function_EMA(vector<double>& arr, double period) {
     average.push_back(EMA_yday);
     double EMA_today;
     for (int i = 1; i < j; i++) {
-
         EMA_today = arr[i] * weight + EMA_yday * (1 - weight);
-
         average.push_back(EMA_today);
-
         EMA_yday = EMA_today;
-
     }
+    EMA_today = average[average.size() - 1] * weight + EMA_yday * (1 - weight);
 
     return average;
-
-
-
-
-
 }
 
-void linearRegression(vector<double>& y) {
+vector <double> linearRegression(vector<double>& y, int period) {
     vector<double> x;
+    vector<double> predict;
+
     for (int i = 0; i < y.size(); i++) {
         x.push_back(i);
-        cout << i;
     }
-    cout << x.size();
 
     double sumX = 0;
     double sumX2 = 0;
@@ -74,14 +65,18 @@ void linearRegression(vector<double>& y) {
     b = (x.size() * sumXY - sumX * sumY) / (x.size() * sumX2 - sumX * sumX);
     a = (sumY - b * sumX) / x.size();
 
-    cout << "Equation of best fit is: y = " << a << " + " << b << "x";
-
+    for (int i = 1; i <= x.size() + period; i++) {
+        predict.push_back(a + b * i);
+    }
+    return predict;
 }
 
-void linearRegressionGradient(vector<double>& y, double it) {
+vector<double> linearRegressionGradient(vector<double>& y, double it, int period) {
 
     vector<double> error;
     vector<double> x;
+    vector<double> predict;
+
     for (int i = 0; i < y.size(); i++) {
         x.push_back(i);
     }
@@ -96,17 +91,87 @@ void linearRegressionGradient(vector<double>& y, double it) {
         err = p - y[idx];
         a = a - alpha * err;
         b = b - alpha * err * x[idx];
-        cout << a << " " << b << endl;
-     
+    }
+    for (int i = 1; i <= x.size() + period; i++) {
+        predict.push_back(a + b * i);
     }
 
+    return predict;
+}
 
+vector<double> polynomialRegression(int grad, vector<double> &values, int period){   
+    vector<double> c(grad+1);
+    vector<double> predict;
+    // Calculare valori xi^2n
+    vector<double> X(2 * grad + 1);
+    for (int i = 0; i < 2 * grad + 1; i++){
+        X[i] = 0;
+        for (int j = 0; j < values.size(); j++){
+            X[i] += pow(j, i);
+        }
+    }
 
-    cout << endl;
+    // Calculare matrice normala augmentata
+    vector < vector <double>> A(grad + 1);
+    for (int i = 0; i < grad + 1; i++)
+       A[i].resize(grad + 2);
 
-    cout << "Final Values are: " << "a=" << a << " " << "b=" << b;
+    for (int i = 0; i <= grad; i++){
+        for (int j = 0; j <= grad; j++) {
+            A[i][j] = X[i + j];
+        }
+    }
+    
+    // Calculare valori xi^n * yi
+    vector<double> Y(grad + 1);
+    for (int i = 0; i < grad + 1; i++){
+        Y[i] = 0;
+        for (int j = 0; j < values.size(); j++){
+            Y[i] += pow(j, i) * values[j];
+        }
+    }
 
+    // Memorare valori pe ultima coloana a matricii A
+    for (int i = 0; i <= grad; i++){
+        A[i][grad + 1] = Y[i];
+    }
+    grad++;
+   
+    // Pivotare matrice A
+    for (int i = 0; i < grad; i++){
+        for (int k = i + 1; k < grad; k++)
+            if (A[i][i] < A[k][i])
+                for (int j = 0; j <= grad; j++) {
+                    double temp = A[i][j];
+                    A[i][j] = A[k][j];
+                    A[k][j] = temp;
+                }
+    }
+    // Eliminarea Gaussiana (creare zerouri sub pivoti)
+    for (int i = 0; i < grad - 1; i++){
+        for (int k = i + 1; k < grad; k++){
+            double t = A[k][i] / A[i][i];
+            for (int j = 0; j <= grad; j++){
+                A[k][j] -= t * A[i][j];
+            }
+        }
+    }
 
+    // UTRIS (Rezolvare Ax = b)
+    for (int i = grad - 1; i >= 0; i--){
+     c[i] = A[i][grad];
+        for (int j = i + 1; j < grad; j++){
+            c[i] -= A[i][j] * c[j];
+        }
+        c[i] /= A[i][i];
+    }
+  
+    for (int i = 1; i <= values.size() + period; i++) {
+        double curentValue = 0;
+        for (int j = 0; j < grad; j++)
+            curentValue += pow(i, j) * c[j];
+        predict.push_back(curentValue);
+   }
 
-
+    return predict;
 }
--- OandaForexP2/OandaForexP2.cpp
+++ OandaForexP2/OandaForexP2.cpp
@@ -1,30 +1,38 @@
-// OandaForexP2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
 #include "Algorithms.h"
-#include "OandaRequests.h"
+#include <iostream>
 #include <vector>
-#include <cpr/cpr.h>
-
 
 using namespace std;
 
 int main()
 {
     vector <double> v = { 1,3,5,6,8,12,18,21,22,25 };
+    
+    
     vector <double> avg = Function_SMA(v, 3);
     for (auto it = avg.begin(); it != avg.end(); it++)
         cout << *it << " ";
     cout << endl;
 
-    vector <double> avg1 = Function_EMA(v, 3);
+    vector <double> avg1 = Function_EMA(v, 5);
     for (auto it = avg1.begin(); it != avg1.end(); it++)
         cout << *it << " ";
-     cout << endl << endl;
+    cout << endl << endl;
 
-     linearRegression(v);
-     linearRegressionGradient(v,100);
+    vector <double> avg2 = linearRegression(v, 3);
+    for (auto it = avg2.begin(); it != avg2.end(); it++)
+        cout << *it << " ";
+    cout << endl << endl;
+   
+    vector <double> avg3 = linearRegressionGradient(v, 100, 3);
+    for (auto it = avg3.begin(); it != avg3.end(); it++)
+        cout << *it << " ";
+    cout << endl << endl;
+    
+    vector <double> avg4 = polynomialRegression(3, v, 3);
+    for (auto it = avg4.begin(); it != avg4.end(); it++)
+        cout << *it << " ";
+    cout << endl << endl;
 
 
     return 0;
